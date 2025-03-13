2f86727e4b93befd367243d8a45ed6fced718c86
murali <murali732000@gmail.com>
2025-03-13 12:17:45 +0000

    add the resource

---
+++ scripts/aws_resource_tracker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+##################################
+
+#Author : Murali
+#Date : 18th sep
+#
+#version -1 
+#
+#
+# The script will report the aws resource usage .
+
+#################################
+## aws cli command  reference 
+set -x  #debug
+# AWS s3
+# AWS EC2
+#AWS lambda 
+# IAM users
+
+# list all the s3 buckets
+
+aws s3 ls
+
+# list all the instances 
+
+aws ec2  describe-instances
+
+
+# list all the aws lambda
+
+aws lambda list-functions
+
+# list all the IAM user 
+
+aws iam list-users 
+
+
+
